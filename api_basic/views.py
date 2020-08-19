31c1159f63a1b7cd91eb026297fe64fd4e29b5f8
shimna <shimnaa.arangil@gmail.com>
2020-08-19 16:40:28 +0530

    generic class based views

--- api_basic/views.py
+++ api_basic/views.py
@@ -1,36 +1,20 @@
 from .models import Article
 from .serializers import ArticleSerializer
-from rest_framework import mixins
 from rest_framework import generics
 
 
-class ArticleList(mixins.CreateModelMixin, mixins.ListModelMixin,
-                  generics.GenericAPIView):
+class ArticleList(generics.ListCreateAPIView):
     #List all Article or create a new article
-
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
-class ArticleDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin, generics.GenericAPIView):
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     #Retreive, update, delete an article
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
 
 
